1f802efeba61586d5e93818fdbc5624f740d3c8d
kaf2000lol <kaf2000@tx.rr.com>
2026-03-09 22:14:59 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,23 @@
+async function generatePlaylist(){
 
+const mood = document.getElementById("moodInput").value;
+const playlistDiv = document.getElementById("playlist");
+const loading = document.getElementById("loading");
+
+playlistDiv.textContent = "";
+loading.textContent = "Generating playlist...";
+
+const response = await fetch("/generate-playlist",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({mood})
+});
+
+const data = await response.json();
+
+loading.textContent = "";
+playlistDiv.textContent = data.playlist;
+
+}
